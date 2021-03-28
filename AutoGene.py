786366c46bb3e8bade0a28b1821867df54eb0889
Sasi Ravipati <51997743+sasir74@users.noreply.github.com>
2021-03-28 13:19:54 +0530

    Update AutoGene.py

--- AutoGene.py
+++ AutoGene.py
@@ -4,12 +4,13 @@ import os
 import csv
 
 df = pd.read_csv('list.csv')
-font = ImageFont.truetype('arial.ttf',90)
+font = ImageFont.truetype('arial.ttf',70)
 font1 = ImageFont.truetype("./MLSJN.TTF",110)
 for index,j in df.iterrows():
     img = Image.open('as.jpg')
     draw = ImageDraw.Draw(img)
-    draw.text(xy=(1500,1215),text='{}'.format(j['name']),fill=(24,19 ,0),font=font1)
-    draw.text(xy=(890,1800),text='{}'.format(j['signature']),fill=(0,0,0),font=font)
-    draw.text(xy=(2100,1800),text='22/03/2021',fill=(0,0,0),font=font) #Change-Daily
-    img.save('pictures/{}.jpg'.format(j['name']))
+    draw.text(xy=(1300,1200),text='{}'.format(j['name']),fill=(24,19 ,0),font=font1)
+    draw.text(xy=(860,1830),text='SYNERGINA',fill=(0,0,0),font=font)
+    draw.text(xy=(2120,1830),text='28/03/2021',fill=(0,0,0),font=font) #Change-Daily
+    img.save('pictures/{}.pdf'.format(j['name']))
+
